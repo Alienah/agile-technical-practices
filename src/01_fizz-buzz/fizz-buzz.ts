71f4f0c0474d269e959acf575d21575e40d31f7a
Aida Adalaber Kairosera <aidalbla@gmail.com>
2019-11-10 20:36:16 +0100

    green: should return Buzz when fizzbuzz 10

--- src/01_fizz-buzz/fizz-buzz.ts
+++ src/01_fizz-buzz/fizz-buzz.ts
@@ -1,7 +1,7 @@
 
 function fizzbuzz(number: number) {
   if ((number % 3) === 0) return 'Fizz';
-  else if (number === 5) return 'Buzz';
+  else if (number % 5 === 0) return 'Buzz';
   else return number.toString();
 };
 
