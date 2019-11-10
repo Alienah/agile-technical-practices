4ef4a0989ed5a362d3c80dc9a8a5d23600d19ee0
Aida Adalaber Kairosera <aidalbla@gmail.com>
2019-11-10 20:17:35 +0100

    green: should return a string representing number 4 when fizzbuzz 4

--- src/01_fizz-buzz/fizz-buzz.ts
+++ src/01_fizz-buzz/fizz-buzz.ts
@@ -1,6 +1,8 @@
 
 function fizzbuzz(number: number) {
-  if (number === 1) return '1'; else return '2';
+  if (number === 1) return '1'; 
+  else if(number === 2) return '2';
+  else return '4';
 };
 
 export default fizzbuzz;
