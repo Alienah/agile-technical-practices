9fda36a11d1f9b70cb1bbf117dabf1032b07e6ba
Aida Adalaber Kairosera <aidalbla@gmail.com>
2019-11-10 20:11:58 +0100

    red: should return a string representing number 2 when fizzbuzz 2

--- tests/01_fizz-buzz/fizz-buzz.spec.ts
+++ tests/01_fizz-buzz/fizz-buzz.spec.ts
@@ -3,8 +3,13 @@ import 'mocha';
 import fizzbuzz from '../../src/01_fizz-buzz/fizz-buzz';
 
 describe('Normal string behaviour', () => { 
-    it('should return a string representing number 1 when fizzbuzz 1', () => { 
-      const result = fizzbuzz(1);
-      expect(result).to.equal('1'); 
-  }); 
+  it('should return a string representing number 1 when fizzbuzz 1', () => { 
+		const result = fizzbuzz(1);
+		expect(result).to.equal('1'); 
+	}); 
+	
+	it('should return a string representing number 2 when fizzbuzz 2', () => {
+		const result = fizzbuzz(2);
+		expect(result).to.equal('2')
+	});
 });
