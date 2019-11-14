fbeb1d49456bb874d37216c84c47d5874056fbb2
Aida Adalaber Kairosera <aidalbla@gmail.com>
2019-11-14 22:53:20 +0100

    red: should return false if number is divisible by 100, but not by 400

--- tests/01_leap-year/leap-year.spec.ts
+++ tests/01_leap-year/leap-year.spec.ts
@@ -6,5 +6,9 @@ describe.only('Leap Year behaviour', () => {
   it('should return true if number is divisible by 4', () => { 
 		const leapYear = isLeapYear(1996);
 		expect(leapYear).to.equal(true); 
+	});  
+  it('should return false if number is divisible by 100, but not by 400', () => { 
+		const leapYear = isLeapYear(1900);
+		expect(leapYear).to.equal(false); 
 	}); 
 });
