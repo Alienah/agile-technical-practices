2556f3dea6ec6e52067dcdcc54a453b192732dab
Aida Adalaber Kairosera <aidalbla@gmail.com>
2019-11-15 19:14:28 +0100

    green: should return false if number is divisible by 100 for atypical common years

--- src/01_leap-year/leap-year.ts
+++ src/01_leap-year/leap-year.ts
@@ -1,5 +1,5 @@
 function isLeapYear(number: number): boolean {
-  return (number % 4) === 0 || (number % 400) === 0 && (number % 100) !== 0;
+  return (number % 400) === 0 || (number % 4) === 0 && (number % 100) !== 0;
 };
 
 export default isLeapYear;
