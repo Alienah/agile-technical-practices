0e81149f8afbdf3e0df2790162d3d8a96292add3
Aida Adalaber Kairosera <aidalbla@gmail.com>
2019-11-19 22:01:25 +0100

    red: should return number 0 when we introduce position 0

--- tests/01_fibonacci/fibonacci.spec.ts
+++ tests/01_fibonacci/fibonacci.spec.ts
@@ -2,10 +2,14 @@ import { expect } from 'chai';
 import 'mocha';
 import getFibonacci from '../../src/01_fibonacci/fibonacci';
 
-describe.only('Fibonacci generator', () => { 
+describe('Fibonacci generator', () => { 
   it('should return a number when we introduce a number', () => { 
 		const result = getFibonacci(1);
 		expect(result).to.be.a('number'); 
+	}); 
+  it('should return number 0 when we introduce position 0', () => { 
+		const result = getFibonacci(0);
+		expect(result).to.equal(0); 
 	}); 
 });
 
